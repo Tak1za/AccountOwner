41c4dd968324612597f95e44ad7c273cfbcc8599
Varun Gupta <varungupta2015135@gmail.com>
2019-09-21 01:54:34 +0530

    Added get routing for owner

---
+++ AccountOwnerServer/Controllers/OwnerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountOwnerServer.Controllers
+{
+    [Route("api/owner")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+
+        public OwnerController(ILoggerManager logger, IRepositoryWrapper repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOwners()
+        {
+            try
+            {
+                var owners = _repository.Owner.GetAllOwners();
+
+                _logger.LogInfo($"Returned all owners from database");
+
+                return Ok(owners);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllOwners action: {ex.Message} ");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOwnerById(Guid id)
+        {
+            try
+            {
+                var owner = _repository.Owner.GetOwnerById(id);
+
+                if (owner.Id.Equals(Guid.Empty))
+                {
+                    _logger.LogError($"Owner with id: {id}, is not present.");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInfo($"Return owner with id: {id}");
+                    return Ok(owner);
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetOwnerById action: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+    }
+}
--- Repository/OwnerRepository.cs
+++ Repository/OwnerRepository.cs
@@ -3,6 +3,7 @@ using Entities;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Repository
@@ -12,6 +13,16 @@ namespace Repository
         public OwnerRepository(RepositoryContext repositoryContext) : base(repositoryContext)
         {
 
+        }
+
+        public IEnumerable<Owner> GetAllOwners()
+        {
+            return FindAll().OrderBy(ow => ow.Name).ToList();
+        }
+
+        public Owner GetOwnerById(Guid ownerId)
+        {
+            return FindByCondition(owner => owner.Id.Equals(ownerId)).DefaultIfEmpty(new Owner()).FirstOrDefault();
         }
     }
 }
