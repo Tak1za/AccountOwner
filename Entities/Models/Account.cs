6e6b90cbfc58a8295946981b3ff4c5147d29d849
Varun Gupta <varungupta2015135@gmail.com>
2019-09-20 23:54:05 +0530

    Added models for the  database

---
+++ Entities/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    [Table("account")]
+    class Account
+    {
+        [Key]
+        public Guid AccountId { get; set; }
+
+        [Required(ErrorMessage = "Date created is required")]
+        public DateTime DateCreated { get; set; }
+
+        [Required(ErrorMessage = "Account type is required")]
+        public string AccountType { get; set; }
+
+        [Required(ErrorMessage = "Owner Id is required")]
+        public Guid OwnerId { get; set; }
+    }
+}
---
+++ Entities/Models/Owner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    [Table("owner")]
+    class Owner
+    {
+        [Key]
+        public Guid OwnerId { get; set; }
+
+        [Required(ErrorMessage="Name is required")]
+        [StringLength(45, ErrorMessage = "Name can't be longer than 45 charachters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(100, ErrorMessage = "Address cannot be longer than 100 charachters")]
+        public string Address { get; set; }
+    }
+}
