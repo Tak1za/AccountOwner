126504c37af0d0fc04f2545cc8802a97bf4909bf
Varun Gupta <varungupta2015135@gmail.com>
2019-09-22 17:20:52 +0530

    Redux setup completed

--- accountownerclientreact/src/index.js
+++ accountownerclientreact/src/index.js
@@ -5,8 +5,12 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import repositoryReducer from './store/reducers/repositoryReducer';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ accountownerclientreact/src/store/actions/repositoryActions.js
@@ -0,0 +1,78 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios/axios';
+
+//GET DATA
+const getDataSuccess = data =>{
+    return {
+        type: actionTypes.GET_DATA_SUCCESS,
+        data: data
+    }
+}
+
+export const getData = (url, props) =>{
+    return (dispatch) =>{
+        axios.get(url).then(response => {
+            dispatch(getDataSuccess(response.data));
+        })
+        .catch(error => {
+            //handle the error when implemented
+        })
+    }
+}
+
+//POST DATA
+const postDataSuccess = data => {
+    return{
+        type: actionTypes.POST_DATA_SUCCESS,
+        response: response
+    }
+}
+
+export const postData = (url, obj, props) => {
+    return (dispatch) => {
+        axios.post(url, obj).then(response => {
+            dispatch(postDataSuccess(response));
+        })
+        .catch(error => {
+            //handle the error when implemented
+        })
+    }
+}
+
+//PUT DATA
+const putDataSuccess = data =>{
+    return{
+        type: actionTypes.PUT_DATA_SUCCESS,
+        response: response
+    }
+}
+
+export const putData = (url, obj, props) =>{
+    return (dispatch) => {
+        axios.put(url, obj).then(response => {
+            dispatch(putDataSuccess(response))
+        })
+        .catch(error => {
+            //handle the error when implemented
+        })
+    }
+}
+
+//DELETE DATA
+const deleteDataSuccess = data => {
+    return{
+        type: actionTypes.DELETE_DATA_SUCCESS,
+        response: response
+    }
+}
+
+export const deleteData = (url, props) =>{
+    return (dispatch) => {
+        axios.delete(url).then(response => {
+            dispatch(deleteDataSuccess(response));
+        })
+        .catch(error => {
+            //handle the error when implemented
+        })
+    }
+}
